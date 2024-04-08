0a2aee2e91030a7e3ff0164835c2ff1b1789f734
Aziz KAVAS <aziz.kavas@airties.com>
2024-04-08 23:42:24 +0300

    Add notes about handling failing tests

--- tests/sportscar/engine/test_engine.py
+++ tests/sportscar/engine/test_engine.py
@@ -10,3 +10,21 @@ def test_engine_functions_as_expected():
 @mark.engine
 def test_fundamental_engine_functions_as_expected(serial_number_from_file, get_engine):
     assert True == get_engine.engine_run_fundamental_functions(serial_number_from_file)
+
+@mark.xfail(reason="Broken after release train #")
+@mark.engine
+def test_engine_type_diesel(get_engine_type):
+    print("\nxfail test is executed")
+    assert "diesel" == get_engine_type
+
+
+@mark.skip(reason="Not a compatible test case")
+@mark.engine
+def test_engine_type_electrical(get_engine_type):
+    print("\nskip test is not executed")
+    assert "electrical" == get_engine_type
+
+@mark.xfail(reason="This future should be deprecated after release trian #")
+@mark.engine
+def test_engine_deprecated_future(get_engine_type):
+    assert False
